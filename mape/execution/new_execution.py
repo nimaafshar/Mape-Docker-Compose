5f60945079f302a71dabf0b6b4c17730135db583
nimaafshar <nima.afsharh@gmail.com>
2020-09-10 19:26:48 +0430

    new execution corrected

--- mape/execution/new_execution.py
+++ mape/execution/new_execution.py
@@ -17,4 +17,4 @@ class DockerExecution(Execution):
         print("Execute\n")
         docker_compose_path = os.getcwd() + '/' + os.getenv("DOCKER_COMPOSE_FILE_DIRECTORY")
         print("docker compose path:",docker_compose_path)
-        p = subprocess.Popen(["docker-compose", "-f",docker_compose_path, "up" ,"-d" ,"--scale", f"picalculator={self.planning.get_decision()}"])
+        p = subprocess.Popen(["docker-compose", "-f",docker_compose_path, "up" ,"-d" ,"--scale", f"picalculator={self.planning.get_decision()}"],shell=True)
