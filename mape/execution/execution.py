c609f5c56ca9eb58e155be74088f465a31410330
nimaafshar <nima.afsharh@gmail.com>
2021-02-07 12:05:10 +0330

    execution base in framework changed

--- mape/execution/execution.py
+++ mape/execution/execution.py
@@ -3,10 +3,16 @@ from abc import abstractmethod, ABC
 
 class Execution(ABC):
     def __init__(self):
-        self.planning = None
+        self.planning = {
+            "optimization": None,
+            "threshold" : None
+        }
 
-    def set_planning(self, planning):
-        self.planning = planning
+    def set_threshold_planning(self, planning):
+        self.planning['threshold'] = planning
+    
+    def set_optimization_planning(self,planning):
+        self.planning['optimization'] = planning
 
     @abstractmethod
     def update(self):
