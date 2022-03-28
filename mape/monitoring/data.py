2f03706b688188f617eab2a5582a353e9cc36a32
nimaafshar <nima.afsharh@gmail.com>
2022-03-28 22:15:15 +0430

    monitoring dataclasses

--- mape/monitoring/data.py
+++ mape/monitoring/data.py
@@ -24,14 +24,17 @@ class SystemMonitoringData(MonitoringData):
         cycle (int): cycle number we that data is generated in, starting from 0.
         cpu_utilization (float): used fraction of the cpu dedicated to monitored service between zero and 1
         memory_utilization (float): used fraction of the memory dedicated to monitored service between zero and 1
+        number_of_containers (int): replicas of
     """
     cpu_utilization: float
     memory_utilization: float
+    replicas: int
 
     def __post_init__(self):
         super(SystemMonitoringData, self).__post_init__()
         assert 0 < self.cpu_utilization < 1, 'cpu utilization is a fraction'
         assert 0 < self.memory_utilization < 1, 'memory utilization is a fraction'
+        assert isinstance(self.replicas, int) > 0 and self.replicas > 0, 'replicas is a positive number'
 
 
 @dataclass
@@ -41,6 +44,19 @@ class APIMonitoringData(MonitoringData):
 
     Args:
         cycle (int): cycle number we that data is generated in, starting from 0.
-        response_time (float): average response time of api in the last monitoring interval
+        avg_response_time (float): average response time of api in the last monitoring interval
+        avg_data_length (float): average data length of requests to api in the last monitoring interval
+        avg_rps (float): average number of requests per second sent to the api in the last monitoring interval
     """
-    response_time: float
+    avg_response_time: float
+    avg_data_length: float
+    avg_rps: float
+
+
+@dataclass
+class CombinedMonitoringData(APIMonitoringData, SystemMonitoringData):
+    """
+    we want both monitoring and system information, so we define a
+    new data type that contains both
+    """
+    pass
