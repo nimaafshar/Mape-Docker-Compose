4ea8207ed10121648ec244737bc67f7a13cdbdc9
nimaafshar <nima.afsharh@gmail.com>
2022-03-30 15:48:04 +0430

    system monitoring

--- mape/monitoring/monitoring.py
+++ mape/monitoring/monitoring.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Optional, Dict, List
+
+import requests
 
 from ..cycle import CycleStep
 from ..execution.data import ExecutionData
@@ -7,6 +9,48 @@ from .data import MonitoringData
 
 
 class Monitoring(CycleStep, ABC):
+    def __init__(self, interval: str):
+        """
+        Args:
+            interval (str): monitoring interval in grafana timedelta format 1m,30s,....
+        """
+        self._interval: str = interval
+
     @abstractmethod
-    def update(self, data: Optional[ExecutionData]) -> Optional[MonitoringData]:
+    def update(self, cycle: int, data: Optional[ExecutionData]) -> Optional[MonitoringData]:
         pass
+
+
+class PrometheusMonitoring(Monitoring, ABC):
+    def __init__(self, interval: str, host: str):
+        """
+        Args:
+            interval (str): monitoring interval in grafana timedelta format 1m,30s,....
+            host (str): prometheus host to query
+        """
+        super().__init__(interval)
+        self._host = host
+
+    def _query_instant_metric(self, querystring: str) -> Optional[List[Dict]]:
+        """
+        Args:
+            querystring (str): prometheus query
+        Returns:
+            None if it could not send query of results were unexpected
+            otherwise prometheus result value
+        """
+        try:
+            response = requests.get(self._host, {'query': querystring})
+        except ConnectionError:
+            return None
+        if response.status_code >= 300 or response.status_code <= 200:
+            return None
+        content: Dict = response.json()
+        if content.get('status') != 'success':
+            return None
+        if content.get('data') is None:
+            return None
+        result = content.get('data').get('result', None)
+        if result is None or len(result) == 0:
+            return None
+        return result[0]['value'][1]
---
+++ mape/monitoring/system_monitoring.py
@@ -0,0 +1,62 @@
+from typing import Optional
+
+from .data import SystemMonitoringData
+from .monitoring import PrometheusMonitoring
+from ..execution import ExecutionData
+
+
+class SystemMonitoring(PrometheusMonitoring):
+    """
+    query service metrics using cadvisor and prometheus
+    """
+
+    def __init__(self, interval: str, host: str, service_name: str):
+        """
+        Args:
+            interval (str): monitoring interval in grafana timedelta format 1m,30s,....
+            host (str): prometheus host to query
+            service_name (str): the service which you want to monitor
+        """
+        super(SystemMonitoring, self).__init__(interval, host)
+        self._service_name: str = service_name
+
+    def _query_cpu_utilization(self) -> Optional[float]:
+        """
+        average cpu utilization of the last monitoring interval between 0 and 1
+        returns None in case of connection problems or absence of data
+        """
+        query: str = 'sum(rate(container_cpu_usage_seconds_total' + \
+                     '{container_label_com_docker_compose_service="' + self._service_name + '"}[{' + self._interval \
+                     + '}]))/sum(container_spec_cpu_quota/' + \
+                     'container_spec_cpu_period{container_label_com_docker_compose_service="' \
+                     + self._service_name + '"})'
+
+        result: Optional[str] = self._query_instant_metric(query)
+        return result if result is None else float(result)
+
+    def _query_memory_utilization(self) -> Optional[float]:
+        """
+        average memory utilization of the last monitoring interval between 0 and 1
+        returns None in case of connection problems or absence of data
+        """
+        query: str = 'sum(container_memory_usage_bytes{container_label_com_docker_compose_service="' + self._service_name + \
+                     '"}/ container_spec_memory_limit_bytes{container_label_com_docker_compose_service="' + self._service_name + \
+                     '"})'
+        result: Optional[str] = self._query_instant_metric(query)
+        return result if result is None else float(result)
+
+    def _query_replicas(self) -> Optional[int]:
+        """
+        number of containers for the target service
+        returns None in case of connection problems or absence of data
+        """
+        query: str = 'count(container_last_seen{container_label_com_docker_compose_service="' + \
+                     self._service_name + '"})'
+        result: Optional[str] = self._query_instant_metric(query)
+        return result if result is None else int(result)
+
+    def update(self, cycle: int, data: Optional[ExecutionData]) -> Optional[SystemMonitoringData]:
+        return SystemMonitoringData(cycle,
+                                    self._query_cpu_utilization(),
+                                    self._query_memory_utilization(),
+                                    self._query_replicas())
