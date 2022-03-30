97c08100fae3bba4d06c6f22a56a5ce75a858c1e
nimaafshar <nima.afsharh@gmail.com>
2022-03-30 16:00:40 +0430

    cycle number added

--- mape/cycle/cycle.py
+++ mape/cycle/cycle.py
@@ -5,7 +5,7 @@ from typing import Any, Optional
 class CycleStep(abc.ABC):
 
     @abc.abstractmethod
-    def update(self, data: Optional) -> Any:
+    def update(self, cycle: int, data: Optional) -> Any:
         """
         update this step using output of the previous step, and return output of this step
         any information about the state of step should be stored in objects memory, not input and output.
