6e56f634c0201a1287b17dbcf9357af2cc4abb9e
nimaafshar <nima.afsharh@gmail.com>
2022-03-31 18:52:06 +0430

    is_complete method to identify incomplete data

--- mape/monitoring/data.py
+++ mape/monitoring/data.py
@@ -1,3 +1,4 @@
+import abc
 from dataclasses import dataclass
 from typing import Optional
 
@@ -12,8 +13,18 @@ class MonitoringData:
     """
     cycle: int
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         assert isinstance(self.cycle, int) and self.cycle >= 0, 'cycle is a non-negative integer'
+
+    @property
+    @abc.abstractmethod
+    def is_complete(self) -> bool:
+        """
+        Returns:
+            true if none of data fields is None,
+            otherwise false
+        """
+        return self.cycle is not None
 
 
 @dataclass
@@ -40,6 +51,13 @@ class SystemMonitoringData(MonitoringData):
         if self.replicas is not None:
             assert isinstance(self.replicas, int) > 0 and self.replicas > 0, 'replicas is a positive number'
 
+    @property
+    def is_complete(self) -> bool:
+        return super(SystemMonitoringData, self).is_complete and \
+               self.cpu_utilization is not None and \
+               self.memory_utilization is not None and \
+               self.replicas is not None
+
 
 @dataclass
 class APIMonitoringData(MonitoringData):
@@ -58,6 +76,14 @@ class APIMonitoringData(MonitoringData):
     avg_rps: Optional[float]
     sensor_count: Optional[int]
 
+    @property
+    def is_complete(self) -> bool:
+        return super(APIMonitoringData, self).is_complete and \
+               self.avg_response_time is not None and \
+               self.avg_data_length is not None and \
+               self.avg_rps is not None and \
+               self.sensor_count is not None
+
 
 @dataclass
 class HybridMonitoringData(MonitoringData):
@@ -71,3 +97,9 @@ class HybridMonitoringData(MonitoringData):
     """
     system: SystemMonitoringData
     api: APIMonitoringData
+
+    @property
+    def is_complete(self) -> bool:
+        return super(HybridMonitoringData, self).is_complete and \
+               self.system.is_complete and \
+               self.api.is_complete
