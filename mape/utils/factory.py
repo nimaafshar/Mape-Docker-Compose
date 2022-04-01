eb686cee342deade6194a84569c2be69c48f4497
nimaafshar <nima.afsharh@gmail.com>
2022-04-01 13:33:29 +0430

    factory added

---
+++ mape/utils/factory.py
@@ -0,0 +1,67 @@
+import datetime
+from typing import Optional
+import json
+import pathlib
+from logging import getLogger
+from jsonschema import validate, ValidationError
+from ..monitoring import HybridMonitoring
+from ..analysis import HybridAnalysis
+from ..optimization import EconomicAdaptationProblemSolver
+from ..optimization.data import SolveArguments, ParetoFrontWeights, EconomicAdaptationParameters
+from ..planning import HybridPlanning
+from ..execution import ScalingExecution
+
+BASE_PATH = pathlib.Path(__file__).parent.parent.resolve()
+
+logger = getLogger()
+
+
+class Factory:
+    _config_schema: Optional[dict] = None
+
+    def __init__(self, config: dict):
+        """
+        Args:
+            config (dic): total config of the application
+        Raises:
+            ValidationError
+        """
+        self._config = config
+        if Factory._config_schema is None:
+            with open(BASE_PATH / 'config_schema.yaml', 'r') as schema_file:
+                Factory._config_schema = json.load(schema_file)
+        try:
+            validate(config, Factory._config_schema)
+        except ValidationError as e:
+            logger.error('config schema validation error.')
+            raise e
+
+    def get_hybrid_monitoring(self) -> HybridMonitoring:
+        return HybridMonitoring(**self._config['monitoring'])
+
+    def get_solve_arguments(self) -> SolveArguments:
+        solve_config = self._config['optimization']['solve'].copy()
+        solve_config['weights'] = ParetoFrontWeights(**solve_config['weights'])
+        return SolveArguments(**solve_config)
+
+    def get_problem_parameters(self) -> EconomicAdaptationParameters:
+        return EconomicAdaptationParameters(**self._config['optimization']['problem'])
+
+    def get_solver(self) -> EconomicAdaptationProblemSolver:
+        return EconomicAdaptationProblemSolver(
+            self.get_solve_arguments(),
+            self.get_problem_parameters()
+        )
+
+    def get_hybrid_analysis(self) -> HybridAnalysis:
+        return HybridAnalysis(self.get_solver(),
+                              **self._config['analysis']['threshold'])
+
+    def get_hybrid_planning(self) -> HybridPlanning:
+        return HybridPlanning(**self._config['planning'])
+
+    def get_scaling_execution(self) -> ScalingExecution:
+        return ScalingExecution(**self._config['execution'])
+
+    def get_mape_interval(self) -> datetime.timedelta:
+        return datetime.timedelta(seconds=self._config['mape']['interval'])
