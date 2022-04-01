bae3a42afe03df6f8d516ff9591b08e88fd7ea52
nimaafshar <nima.afsharh@gmail.com>
2022-04-01 13:33:48 +0430

    driver code

--- mape/driver.py
+++ mape/driver.py
@@ -1,11 +1,25 @@
-from dotenv import load_dotenv
+import yaml
+from logging import getLogger
 
-load_dotenv()
+from utils import Factory, BASE_PATH
+from cycle import MAPECycle
+
+logger = getLogger()
 
 
 def main():
-    # driver code
-    pass
+    with open(BASE_PATH / 'config.yaml', 'r') as config_file:
+        total_config: dict = yaml.load(config_file, Loader=yaml.SafeLoader)
+    logger.info('configuration loaded.')
+    factory: Factory = Factory(total_config)
+    cycle: MAPECycle = MAPECycle(
+        factory.get_hybrid_monitoring(),
+        factory.get_hybrid_analysis(),
+        factory.get_hybrid_planning(),
+        factory.get_scaling_execution(),
+        factory.get_mape_interval()
+    )
+    cycle.run()
 
 
 if __name__ == "__main__":
